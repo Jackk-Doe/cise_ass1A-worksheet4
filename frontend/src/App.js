79795ac96116b94010181deeb1f2426019c9a5b4
Jackk-Doe <dodo-stv@hotmail.com>
2022-04-08 13:55:08 +1200

    Created Page Router
    - Created pages to use in Router
    - Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-// import Home from "./page/Home";
-// import SEPractice from "./pages/SE-Practice";
-// import SubmitArticle from "./pages/Submit-Article"; 
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
 // import NotFoundPage from "./pages/404";
 
 const App = () => {
@@ -12,12 +12,14 @@ const App = () => {
       <div>
         <h1>Software Engineering Practice Evidence Resposiotry (SEPER)</h1>
         <ul className="header">
-          <li><a href="/">Home</a></li>
-          <li><a href="/SEPractice">Select the Practice</a></li>
-          <li><a href="/SubmitArticle">Submit an Article</a></li>
+          <li><NavLink exact to="/">Home</NavLink></li>
+          <li><NavLink to="/SEPractice">Select the Practice</NavLink></li>
+          <li><NavLink to="/SubmitArticle">Submit an Article</NavLink></li>
         </ul>
         <div className="content">
-          <div></div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/SEPractice" component={SEPractice}/>
+          <Route path="/SubmitArticle" component={SubmitArticle}/>
         </div>
       </div>
     </Router>
